175b6f8c2dbc8565ecd78165cbd5921db812d8b6
unknown <bhotnikhil25@gmail.com>
2023-06-29 23:28:50 +0530

    [Nikhil]-Stack Implmetation

---
+++ StackImplementation/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StackImplementation
+{
+    /*Ability to create a Stack of
+            56->30->70
+            - Use LinkedList to do the Stack Operations
+            - Here push will internally call add method on LinkedList.
+            - So 70 will be added first then 30 and then 56 to make 56 on top of the Stack
+     */
+
+    /*Ability to peak and pop from the Stack till it is empty
+     * 56 ->30 ->70
+
+        - Use LinkedList to do the Stack Operations
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("Top element of the stack: " + stack.Peek());
+
+            Console.WriteLine("Elements in the stack:");
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
---
+++ StackImplementation/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementation
+{
+    public class Stack
+    {
+        private Node top;
+
+        public Stack()
+        {
+            top = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            int data = top.Data;
+            top = top.Next;
+            return data;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            return top.Data;
+        }
+    }
+}
